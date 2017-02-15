ab056a3721c9ad611030a8adc05b2e8cc98e0962
Renan Verissimo de V. Souza <souza.sup.erp@alterdata.matriz>
2017-02-15 14:46:58 -0200

    Inserção do timer + ajustes Js

---
+++ app/scripts/model/timer_model.js
@@ -0,0 +1,17 @@
+(function(){
+  'use strict'
+
+  angular.module('FaltaPouco')
+  .factory([function(){
+    class Timer{
+      constructor(){
+        //continuar posteriormente!
+        this.algo = algo;
+      }
+      func(){
+        //assim que se declara uma func bitches!
+      }
+    }
+    return Timer;
+  }])
+}())
