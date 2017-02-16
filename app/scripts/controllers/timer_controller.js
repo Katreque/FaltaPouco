42ead36ffde6db5e4f0e8305dea62c13a403d5c8
Renan Verissimo de V. Souza <souza.sup.erp@alterdata.matriz>
2017-02-16 15:35:34 -0200

    Funções básicas de countdown adicionadas.

--- app/scripts/controllers/timer_controller.js
+++ app/scripts/controllers/timer_controller.js
@@ -3,5 +3,26 @@
 
   angular.module('FaltaPouco')
     .controller('controllerTimer', [function(){
+      var self = this;
+
+      var timer = new Timer();
+      var flag = 0;
+
+      self.triggerTimer = function(){
+        if(flag === 0){
+          flag++;
+          return timer.start({countdown: true, startValues: {hours:8 }});
+        }
+        flag--;
+        return timer.pause();
+      }
+
+      timer.addEventListener('secondsUpdated', function (e) {
+        $('#timer').html(timer.getTimeValues().toString());
+      });
+
+      timer.addEventListener('started', function (e) {
+        $('#timer').html(timer.getTimeValues().toString());
+      });
     }])
 }())
