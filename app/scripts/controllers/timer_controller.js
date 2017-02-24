353c17581e0a383d993bf5897deae620fd8cd38c
Renan Verissimo de V. Souza <souza.sup.erp@alterdata.matriz>
2017-02-24 18:12:32 -0300

    node-notifier added + inicio da implementação

--- app/scripts/controllers/timer_controller.js
+++ app/scripts/controllers/timer_controller.js
@@ -1,6 +1,8 @@
 (function(){
   'use strict'
 
+  const notifier = require('node-notifier')
+
   angular.module('FaltaPouco')
     .controller('controllerTimer', ['timerModel', 'VALOR_INIT', '$location', function(timerModel, VALOR_INIT, $location){
       var self = this;
@@ -23,6 +25,10 @@
 
       timer.addEventListener('secondsUpdated', function (e) {
         $('#timer').html(timer.getTimeValues().toString());
+        if(timer.getTimeValues().hours === (parseInt(VALOR_INIT) - 1)){
+          //Criar um service para trabalhar com esses eventos corretamente.
+          notifier.notify('Birrrrllllllll! Uma hora de trabalho se passou!');
+        }
       });
 
       timer.addEventListener('started', function (e) {
